0d297ec4ad6206099bc3d0bd002f4d59c70d5b55
sqchai <chaisiqi1999@gmail.com>
2022-10-18 04:43:01 +0000

    customize

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 # CHECKPOINT_PATH=ckpts/fiery.ckpt
-CONFIG=fiery/configs/single_timeframe.yml
+CONFIG=fiery/configs/literature/static_lss_setting.yml
 NUSCENES_DATAROOT=/mnt/datasets/nuscenes
 
 python train.py --config ${CONFIG} DATASET.DATAROOT ${NUSCENES_DATAROOT}
